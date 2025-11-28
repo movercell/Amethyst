5a0dc2528d8f5f067adaa993677a3af158b2b19d
The Mover Cell <73055569+movercell@users.noreply.github.com>
2025-11-28 16:45:00 +0700

    Added docs for Camera interface

--- engineheaders/graphics/Camera.h
+++ engineheaders/graphics/Camera.h
@@ -7,19 +7,24 @@ const float CAMERA_DEFAULT_SPEED       =  2.5f;
 const float CAMERA_DEFAULT_SENSITIVITY =  0.1f;
 const float CAMERA_DEFAULT_FOV         =  120.0f;
 
+/*!
+* \brief A camera interface.
+*/
 class Camera {
 public:
-    // Camera attributes
+    // Camera attributes.
     vec3 Position;
-    // Euler angles
+    // Euler angles.
     float Yaw   = CAMERA_DEFAULT_YAW;
     float Pitch = CAMERA_DEFAULT_PITCH;
     // Camera options
     float MouseSensitivity = CAMERA_DEFAULT_SENSITIVITY;
     float FOV              = CAMERA_DEFAULT_FOV;
     vec2 Resolution;
-    // Coordinate axi
+    //! Coordinate front axis.
     vec3 Front = vec3(1.0f, 0.0f, 0.0f);
+    //! Coordinate up axis.
     vec3 Up;
+    //! Coordinate right axis.
     vec3 Right;
 };
