ea40a8b9f7dfd84730be093aad8e902334fa925b
The Mover Cell <73055569+movercell@users.noreply.github.com>
2026-06-23 19:12:03 +0700

    Better temporary texture

--- testgame/scripts/shaders/opengl/generic.vs
+++ testgame/scripts/shaders/opengl/generic.vs
@@ -3,13 +3,23 @@
 #include "STDGLModel.incl"
 #include "STDGLCamera.incl"
 
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 Position;
+layout (location = 1) in vec3 Normal;
+layout (location = 2) in vec2 UV;
 out gl_PerVertex {
     vec4 gl_Position;
     float gl_PointSize;
 };
+out VertexData {
+    vec3 Position;
+    vec3 Normal;
+    vec2 UV;
+} toFrag;
 
 
 void main() {
-    gl_Position = CameraViewProjection * InstanceMatrices[InstanceIndices[gl_BaseInstance][gl_InstanceID]] * vec4(aPos, 1.0f);
+    toFrag.Position = Position;
+    toFrag.Normal = Normal;
+    toFrag.UV = UV;
+    gl_Position = CameraViewProjection * InstanceMatrices[InstanceIndices[gl_BaseInstance][gl_InstanceID]] * vec4(Position, 1.0f);
 }
--- testgame/scripts/shaders/opengl/unlitgeneric.fs
+++ testgame/scripts/shaders/opengl/unlitgeneric.fs
@@ -1,5 +1,20 @@
 #version 460 core
 out vec4 FragColor;
+in VertexData {
+    vec3 Position;
+    vec3 Normal;
+    vec2 UV;
+};
+
+vec4 GenerateTemporaryTexture() {
+    uint isOther = 0u;
+    if (UV.x >= 0.5f)
+        isOther = 1u;
+    if (UV.y >= 0.5f)
+        isOther += 2u;
+    vec3 ret = vec3(1.0f);
+    return isOther != 0u && isOther != 3u ? vec4(ret.xyz, 1.0f) : vec4((ret * 0.5f).xyz, 1.0f);
+}
 
 void main()
 {
@@ -7,7 +22,6 @@ void main()
     const float far = 32768.0f;
     float linearDepth = (2.0 * near * far) / (far + near - gl_FragCoord.z * (far - near));
     linearDepth /= 100.0f;
-    FragColor = vec4(0.3f * linearDepth, 0.3f * linearDepth, 0.7f * linearDepth, 1.0f);
-    //FragColor = vec4(0.3f, 0.3f, 0.7f, 1.0f);
+    FragColor = GenerateTemporaryTexture() * vec4(0.3f * linearDepth, 0.3f * linearDepth, 0.7f * linearDepth, 1.0f);
 }
 
