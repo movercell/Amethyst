3a78d3948821dd5c57acb781f5826df216dd7280
The Mover Cell <73055569+movercell@users.noreply.github.com>
2026-06-28 20:26:37 +0700

    Improved lights

--- src/graphics/opengl/STDGLRenderer.cpp
+++ src/graphics/opengl/STDGLRenderer.cpp
@@ -91,7 +91,7 @@ void STDGLRenderer::Draw() {
             InstanceArrayRefs.emplace_back(iarray);
         
         glEnable(GL_POLYGON_OFFSET_FILL);
-        glPolygonOffset(1.7f, 1.2f);
+        glPolygonOffset(1.3f, 0.9f);
         glDisable(GL_CULL_FACE);
         for (auto light : rworld->lightsystem.LightResources) {
             if (!light) continue;
--- testgame/scripts/shaders/opengl/generic.fs
+++ testgame/scripts/shaders/opengl/generic.fs
@@ -17,14 +17,14 @@ in VertexData {
     vec2 UV;
 };
 
-vec4 GenerateTemporaryTexture() {
+vec3 GenerateTemporaryTexture() {
     uint isOther = 0u;
     if (UV.x >= 0.5f)
         isOther = 1u;
     if (UV.y >= 0.5f)
         isOther += 2u;
     vec3 ret = vec3(1.0f);
-    return isOther != 0u && isOther != 3u ? vec4(ret.xyz, 1.0f) : vec4((ret * 0.9f).xyz, 1.0f);
+    return isOther != 0u && isOther != 3u ? ret : ret * 0.9f;
 }
 
 void main()
@@ -35,19 +35,8 @@ void main()
     //linearDepth /= 100.0f;
 
     float linearDepth = 1.0f;
-    vec4 Albedo = GenerateTemporaryTexture() * vec4(0.3f * linearDepth, 0.3f * linearDepth, 0.7f * linearDepth, 1.0f);
+    vec3 Albedo = GenerateTemporaryTexture() * vec3(0.3f * linearDepth, 0.3f * linearDepth, 0.7f * linearDepth);
 
-    vec4 LightPos = LightBuffer.Lights[0].ViewProjection * Position;
-    vec3 projCoords = LightPos.xyz / LightPos.w;
-    projCoords = projCoords * 0.5 + 0.5;
-
-    float NormalDotLightDir = max(dot(normalize(Normal), normalize(LightBuffer.Lights[0].LightPos - vec3(Position.xyz))), 0.0f);
-    
-    float Shadow = SampleSpotLightForShadows(0, projCoords, NormalDotLightDir);
-
-    Shadow += 0.3f;
-    Shadow = min(1.0f, Shadow);
-
-    FragColor = Albedo * vec4(Shadow, Shadow, Shadow, 1.0f);
+    FragColor = vec4(STDGLight_ProcessSpotlight(Albedo, normalize(Normal), 0.05f, 0.7f, vec3(Position.xyz), 0), 1.0f);
     FragNormal = Octahedral_Map(normalize(Normal));
 }
