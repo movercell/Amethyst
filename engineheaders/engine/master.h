dc007f12f1e7fe70b3c4760d758f6812216411b0
The Mover Cell <73055569+movercell@users.noreply.github.com>
2026-07-08 20:21:03 +0700

    Stole the quaternion multiplicaton formula from godot and now it all works

--- engineheaders/engine/master.h
+++ engineheaders/engine/master.h
@@ -333,17 +333,17 @@ struct alignas(sizeof(float) * 4) quat {
 
     quat operator*(const quat& other) {
         return quat(
-            w * other.x - x * other.w - y * other.z + z * other.y,
-            w * other.y + x * other.z - y * other.w - z * other.x,
-            w * other.z - x * other.y + y * other.x - z * other.w,
-            w * other.w + x * other.x + y * other.y + z * other.z
+            w * other.x + x * other.w + y * other.z - z * other.y,
+            w * other.y + y * other.w + z * other.x - x * other.z,
+            w * other.z + z * other.w + x * other.y - y * other.x,
+            w * other.w - x * other.x - y * other.y - z * other.z
         );
     }
     quat& operator*=(const quat& other) {
-        *this = quat(w * other.x - x * other.w - y * other.z + z * other.y,
-                     w * other.y + x * other.z - y * other.w - z * other.x,
-                     w * other.z - x * other.y + y * other.x - z * other.w,
-                     w * other.w + x * other.x + y * other.y + z * other.z);
+        *this = quat(w * other.x + x * other.w + y * other.z - z * other.y,
+                     w * other.y + y * other.w + z * other.x - x * other.z,
+                     w * other.z + z * other.w + x * other.y - y * other.x,
+                     w * other.w - x * other.x - y * other.y - z * other.z);
         
         return *this;
     }
--- src/entities/MiscEntities.cpp
+++ src/entities/MiscEntities.cpp
@@ -2,16 +2,8 @@
 
 
 void PointRotating::Update() {
-    angles += rotationamount * deltaTime;
+    rotation *= quat(rotationamount * deltaTime);
+    rotation.Norm();
 
-    if (angles.x > 360.0f) angles.x = 0.0f;
-    if (angles.y > 360.0f) angles.y = 0.0f;
-    if (angles.z > 360.0f) angles.z = 0.0f;
-
-    if (angles.x < 0.0f) angles.x = 360.0f;
-    if (angles.y < 0.0f) angles.y = 360.0f;
-    if (angles.z < 0.0f) angles.z = 360.0f;
-
-    rotation = quat(angles);
     BaseEntity::Update();
 }
