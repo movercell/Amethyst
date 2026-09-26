64b48f67040b9b35d78ebe4cad33e708f59c8b8c
The Mover Cell <73055569+movercell@users.noreply.github.com>
2026-09-26 21:55:34 +0700

    Better variable names

--- src/engineui/BaseEngineUI.cpp
+++ src/engineui/BaseEngineUI.cpp
@@ -19,6 +19,8 @@ void Engine::DrawEngineUI() {
 std::map<std::string_view, World*>& GetWorldMap();
 
 inline constexpr float RunButtonSize = 30.0f;
+inline constexpr float InInputSize = 80.0f;
+inline constexpr float AsInputSize = 30.0f;
 
 void Engine::DrawConsole() {
     auto& WorldMap = GetWorldMap();
@@ -43,25 +45,25 @@ void Engine::DrawConsole() {
 
     ImGui::AlignTextToFramePadding();
 
+    static int InInput = 0;
     ImGui::Text("In:");
     ImGui::SameLine();
-    static int In = std::numeric_limits<int>::max();
-    if (In >= WorldMap.size()) { // In case the world stops to exist and now we are out of bounds on the map.
-        In = 0;
+    if (InInput >= WorldMap.size()) { // In case the world stops to exist and now we are out of bounds on the map.
+        InInput = 0;
     }
     std::string InPreview;
     {
         auto InVal = WorldMap.begin();
-        for (int i = 0; i < In; i++) ++InVal;
+        for (int i = 0; i < InInput; i++) ++InVal;
         InPreview = InVal->second->GetName();
     }
-    ImGui::SetNextItemWidth(60.0f);
+    ImGui::SetNextItemWidth(InInputSize);
     if (ImGui::BeginCombo("##AmethystConsoleInputIn", InPreview.c_str())) {
         int i = 0;
         for (const auto& world : WorldMap) {
             const std::string& Name = world.second->GetName();
-            if (ImGui::Selectable(Name.c_str(), In == i)) {
-                In = i;
+            if (ImGui::Selectable(Name.c_str(), InInput == i)) {
+                InInput = i;
                 InPreview = Name;
             }
             i++;
@@ -72,11 +74,11 @@ void Engine::DrawConsole() {
     ImGui::SameLine();
 
 
-    static std::string As = "0";
+    static std::string AsInput = "0";
     ImGui::Text("As:");
     ImGui::SameLine();
-    ImGui::SetNextItemWidth(20.0f);
-    ImGui::InputText("##AmethystConsoleInputAs", &As, ImGuiInputTextFlags_EnterReturnsTrue);
+    ImGui::SetNextItemWidth(AsInputSize);
+    ImGui::InputText("##AmethystConsoleInputAs", &AsInput, ImGuiInputTextFlags_EnterReturnsTrue);
     ImGui::SameLine();
 
     bool ShouldDoCommand = false;
@@ -93,13 +95,13 @@ void Engine::DrawConsole() {
     if (ImGui::Button("Go!", ImVec2(RunButtonSize, 0.0f))) ShouldDoCommand = true;
 
     if (ShouldDoCommand) {
-        auto InVal = WorldMap.begin();
-        for (int i = 0; i < In; i++) ++InVal;
+        auto In = WorldMap.begin();
+        for (int i = 0; i < InInput; i++) ++In;
         
-        int AsVal;
-        std::from_chars(As.data(), As.data() + As.size(), AsVal);
+        int As;
+        std::from_chars(AsInput.data(), AsInput.data() + AsInput.size(), As);
 
-        Engine::ExecuteConsoleCommand(InVal->second, AsVal, Do);
+        Engine::ExecuteConsoleCommand(In->second, As, Do);
         Do.clear();
     }
 
